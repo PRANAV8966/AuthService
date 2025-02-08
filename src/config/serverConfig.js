c415cffecb987c5d250c39f16983d857f6a5b987
PRANAV8966 <pranav.wan706@gmail.com>
2025-02-08 21:41:11 +0530

    added createAndValidateToken func in service layer

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -3,4 +3,5 @@ dotenv.config();
 
 module.exports = {
     PORT: process.env.PORT,
+    jwtKey: process.env.jwtKey
 }
--- src/servies/user-service.js
+++ src/servies/user-service.js
@@ -1,5 +1,8 @@
 const UserRepository   = require("../repositories/user-repository"); 
 
+const jwt = require('jsonwebtoken');
+const { jwtKey } = require('../config/serverConfig')
+
 class userService {
 
     constructor() {
@@ -12,6 +15,26 @@ class userService {
             return newUser;
         } catch (error) {
             console.log("something went wrong in the user service");
+            throw error;
+        }
+    }
+
+    createToken(user) {
+        try {
+            const token = jwt.sign(user, jwtKey, {expiresIn: '1h'});
+            return token;
+        } catch (error) {
+            console.log("failed to create token");
+            throw error;
+        }
+    }
+
+    verifyToken(token) {
+        try {
+            const response = jwt.verify(token, jwtKey);
+            return response;
+        } catch (error) {
+            console.log("failed to validate token", error);
             throw error;
         }
     }
